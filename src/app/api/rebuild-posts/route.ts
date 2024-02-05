d38bdb196bb91d457edce0116442e5318d021f09
Xeradream <99032814+Xeradream@users.noreply.github.com>
2024-02-05 15:46:58 +0100

    Question 3.6

---
+++ src/app/api/rebuild-posts/route.ts
@@ -0,0 +1,22 @@
+import { revalidatePath } from "next/cache"
+import { NextResponse } from "next/server"
+
+export async function POST(req : Request) {
+    const keyWebhook = process.env.SUPABASE_WEBHOOK_KEY
+    const keyApi = req.headers.get("api-key")
+    if (keyApi === null) {
+        return new Response('"API-Key" header is missing', {
+            status: 401
+        })
+    } else if (keyApi !== keyWebhook) {
+        return new Response("API Key is not valid", {
+            status: 403
+        })
+    }
+    console.log("test")
+    revalidatePath('/[categorySlug]/[productSlug]')
+    return NextResponse.json({
+        "revalidated":true,
+        "date":new Date().toISOString()
+    })
+}
